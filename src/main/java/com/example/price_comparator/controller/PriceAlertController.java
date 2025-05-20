5ae63ae4b72d72d491b26d809bad1064cae166fe
Robert <mirearobert32@gmail.com>
2025-05-20 14:37:32 +0300

    Add test alert

--- src/main/java/com/example/price_comparator/controller/PriceAlertController.java
+++ src/main/java/com/example/price_comparator/controller/PriceAlertController.java
@@ -3,6 +3,8 @@ package com.example.price_comparator.controller;
 import com.example.price_comparator.model.PriceAlert;
 import com.example.price_comparator.model.PriceEntry;
 import com.example.price_comparator.service.PriceService;
+import com.example.price_comparator.util.PriceAlertScheduler;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,10 +16,10 @@ import java.util.List;
 public class PriceAlertController {
 
     private final PriceService priceService;
+    private final PriceAlertScheduler priceAlertScheduler;
 
     @GetMapping("/check")
-    public List<PriceEntry> checkAlert(@RequestParam String productName, @RequestParam double targetPrice) {
-        PriceAlert alert = new PriceAlert(productName, targetPrice, "dummy@example.com");
+    public List<PriceEntry> checkAlert(@RequestBody PriceAlert alert) {
         return priceService.checkAlerts(alert);
     }
 
@@ -25,6 +27,11 @@ public class PriceAlertController {
     public String addAlert(@RequestBody PriceAlert alert) {
         priceService.addAlert(alert);
         return "Price alert added successfully!";
+    }
+
+    @GetMapping("/test")
+    public void testAlerts(){
+        priceAlertScheduler.checkAndNotifyAlerts();
     }
 
 }
